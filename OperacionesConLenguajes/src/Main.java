0fbbe811859f47475e607c4bccf6c24a4a932b6a
Enrique Flores Ortiz <floresnenrique1@gmail.com>
2024-02-28 09:41:07 -0600

    Segundo Commit

--- OperacionesConLenguajes/src/Main.java
+++ OperacionesConLenguajes/src/Main.java
@@ -10,14 +10,14 @@ public class Main {
         String[] cadena1 = {"10", "20", "40", "50"};
         String[] cadena2 = {"10", "40", "30", "60" , "80"};
 
-        LenguajesRegulares lenguajesRegulares = new LenguajesRegulares(lenguaje1);
-        lenguajesRegulares.GenerarPrefijos(lenguaje1);
-        lenguajesRegulares.GenerarSufijos(lenguaje1);
-        LenguajesRegulares.unionDecadenas(cadena1, cadena2);
-        LenguajesRegulares.interseccionDecadenas(cadena1, cadena2);
-        LenguajesRegulares.GenerarConcatenacion(cadena1, cadena2);
-        LenguajesRegulares.GenerarPotencia(cadena1);
-        LenguajesRegulares.GenerarPotencia(cadena2);
+        OperacionLenguajes operacionLenguajes = new OperacionLenguajes(lenguaje1);
+        operacionLenguajes.GenerarPrefijos(lenguaje1);
+        operacionLenguajes.GenerarSufijos(lenguaje1);
+        OperacionLenguajes.unionDecadenas(cadena1, cadena2);
+        OperacionLenguajes.interseccionDecadenas(cadena1, cadena2);
+        OperacionLenguajes.GenerarConcatenacion(cadena1, cadena2);
+        OperacionLenguajes.GenerarPotencia(cadena1);
+        OperacionLenguajes.GenerarPotencia(cadena2);
 
     }
 }
